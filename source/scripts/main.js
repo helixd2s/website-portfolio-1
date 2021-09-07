9d25012bcba01ddc1f297237c5297e27920513ad
helixd-2k18 <helixd.2k18@gmail.com>
2021-09-07 23:37:58 +0000

    add block, problems

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -57,3 +57,34 @@ const reading = new Swiper('.reading .swiper', {
     el: '.reading .swiper-scrollbar',
   },
 });
+
+//
+const customers = new Swiper('.customers .swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+  modules: [Navigation, Pagination],
+  slidesPerView: 2,
+  spaceBetween: 64,
+
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.customers .swiper-pagination',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.customers .swiper-scrollbar',
+  },
+});
